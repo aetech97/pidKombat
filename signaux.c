2695ea4700e373652bc5c415dedae4505caef5b4
Benji-de-Marseille <benjamin.bortolotti@isen.yncrea.fr>
2020-12-04 16:22:57 +0100

    MàJ

--- signaux.c
+++ signaux.c
@@ -4,9 +4,18 @@
 //  Created by Benjamin Bortolotti on 04/12/2020.
 //
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/wait.h>
+
+int SendSIG(pid_t PID, int Signal)
+//Envoi signal [SIG_QUIT, SIGUSR1, SIGUSR2] au PID avec kill()
+
+{
+    printf("PAN!!!!\n", Signal, PID);
+    kill(PID,Signal);
+    wait(NULL);
+}
+
 
